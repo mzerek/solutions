5fd7508fa4ab724feffeb378490d4c356ab694e2
M Z <maciek.zerek@gmail.com>
2023-05-19 16:55:26 +0200

    Count trailing zeroes in factorial of a number

---
+++ src/src/main/java/org/mzerek/solutions/factorialtrailingzeroes/SolutionTrailingZeroesFactorial.java
@@ -0,0 +1,60 @@
+package org.mzerek.solutions.factorialtrailingzeroes;
+
+import java.math.BigInteger;
+
+//https://www.purplemath.com/modules/factzero.htm
+//https://www.geeksforgeeks.org/count-trailing-zeroes-factorial-number/
+
+public class SolutionTrailingZeroesFactorial {
+
+    public static void main(String[] args) {
+        int number = 5;
+        BigInteger bigNumber = BigInteger.valueOf(number);
+        BigInteger factorial = factorialExample(bigNumber);
+
+        System.out.println("Factorial from " + number + " is: " + factorial);
+
+        int number2 = 5;
+        int countZeroes = findTrailingZeros(number2);
+        System.out.println("Trailing zeroes for " + number2 + " is: " + countZeroes);
+
+    }
+
+    public static int findTrailingZeros(int n) {
+        if(n < 0) {
+            return -1;
+        }
+
+        int counter = 0;
+
+        for (int i = 5; n / i >= 1; i *=5) {
+            System.out.println(" ");
+            System.out.println("begin ================");
+            System.out.println("i= " + i);
+            System.out.println("counter= " + counter);
+
+            counter = counter + n / i;
+
+            System.out.println("counter= " + counter);
+            System.out.println("end  ================");
+        }
+
+        return counter;
+    }
+
+
+
+
+
+
+    public static BigInteger factorialExample(BigInteger number) {
+
+        BigInteger factorial = BigInteger.valueOf(1);
+
+        for(int i=1; i <= number.intValue(); i++) {
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+
+        return factorial;
+    }
+}
